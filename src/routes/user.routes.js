bf7af6fa0c22dbcd96049bc34dc7a18049cfe14b
Tran Mai Trung Nguyen <liduc1234@gmail.com>
2025-04-10 22:32:46 +0700

    Update Role

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,30 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
-const { isAdmin, isOwnerOrAdmin } = require('../middlewares/auth.middleware');
+const { UserRole } = require('../constants/roles');
+const checkRole = require('../middlewares/checkRole');
+const checkOwnership = require('../middlewares/checkOwnership');
+const User = require('../models/user.model');
 
 // Lấy tất cả users (phân trang) - chỉ admin
-router.get('/', isAdmin, userController.getAllUsers);
+router.get('/', checkRole([UserRole.ADMIN]), userController.getAllUsers);
 
 // Lấy user theo ID - admin hoặc chính user đó
-router.get('/:id', isOwnerOrAdmin, userController.getUserById);
+router.get('/:id', checkOwnership(User), userController.getUserById);
 
 // Tạo user mới - chỉ admin
-router.post('/', isAdmin, userController.createUser);
+router.post('/', checkRole([UserRole.ADMIN]), userController.createUser);
 
 // Cập nhật user - admin hoặc chính user đó
-router.put('/:id', isOwnerOrAdmin, userController.updateUser);
+router.put('/:id', checkOwnership(User), userController.updateUser);
 
 // Xóa user - chỉ admin
-router.delete('/:id', isAdmin, userController.deleteUser);
+router.delete('/:id', checkRole([UserRole.ADMIN]), userController.deleteUser);
 
 // Tìm kiếm user - chỉ admin
-router.get('/search', isAdmin, userController.searchUsers);
+router.get('/search', checkRole([UserRole.ADMIN]), userController.searchUsers);
 
 // Thay đổi trạng thái user - chỉ admin
-router.put('/:id/toggle-status', isAdmin, userController.toggleUserStatus);
+router.put('/:id/toggle-status', checkRole([UserRole.ADMIN]), userController.toggleUserStatus);
 
 // Cập nhật thông tin cá nhân - chính user đó
-router.put('/profile', isOwnerOrAdmin, userController.updateProfile);
+router.put('/profile', checkOwnership(User), userController.updateProfile);
 
 module.exports = router; 
